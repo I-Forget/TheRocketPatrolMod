6cc7938c4569e7d95121f01af79f45a53c5f8761
bdvictor <bdvictor@ucsc.edu>
2021-04-21 07:38:16 -0700

    allowed movement while firing; dirty but works 5

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -10,6 +10,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
     update() {
         if(this.isFiring){
             this.y -= this.movementSpeed;
+            if(keyLEFT.isDown){
+                this.x -= this.movementSpeed;
+            }
+            if(keyRIGHT.isDown){
+                this.x += this.movementSpeed;
+            }
             if(this.y < borderUISize * 3){
                 this.reset();
             }
